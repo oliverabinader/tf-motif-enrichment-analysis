c1b1066dd0c1432b40ab6b5161f17a0c4c395383
oliverabinader <78950087+oliverabinader@users.noreply.github.com>
2026-04-13 16:19:59 -0400

    enrichr_analysis.R

--- enrichr_analysis.R
+++ enrichr_analysis.R
@@ -7,17 +7,20 @@ result <- a$libraryName[grep("chea|encode", a$libraryName, ignore.case = TRUE)]
 dbs <- c("ENCODE_and_ChEA_Consensus_TFs_from_ChIP-X")
 
 # Load differential expression results
-data1 <- read_excel("path/file")
+data1 <- read_excel("/path/to/input_file")
+#data1 <- readr::read_tsv("/path/to/input_file", col_names = TRUE, quote = "")
 #data1 <- as.data.frame(data1)
-#also check for N/A values in the dataset
+#also check for N/A values in the dataset: colSums(is.na(data1)) #Count NAs per column 
 
 # Filter upregulated genes
 deg_up <- data1[ data1$log2FoldChange >= 1 & data1$padj < 0.05, ]
+write.table(deg_up, file = "DEGs_upregulated_2FC_FDR_less_than_0.05.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
 # Filter downregulated genes
 deg_down <- data1[ data1$log2FoldChange <= -1 & data1$padj < 0.05, ]
+write.table(deg_down, file = "DEGs_downregulated_2FC_FDR_less_than_0.05.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
 
 # Run Enrichr
-enriched_up <- enrichr(unique(deg_up$gene), dbs)
+enriched_up <- enrichr(unique(deg_up$gene), dbs) #Character vector of Entrez gene symbols as input
 enriched_down <- enrichr(unique(deg_down$gene), dbs)
 
 # Export results
